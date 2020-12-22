0aed1f77f2cbfd505997cff5eeed7ad8bc59b1e4
zouyuqin <2395839021@qq.com>
2020-12-22 19:55:35 +0800

    wdnmd

--- src/main/java/com/zou/zyqblogs/config/hello.java
+++ src/main/java/com/zou/zyqblogs/config/hello.java
@@ -2,6 +2,6 @@ package com.zou.zyqblogs.config;
 
 public class hello {
     public static void main(String[] args) {
-        System.out.println("sout1");
+        System.out.println("soutdasd");
     }
 }
